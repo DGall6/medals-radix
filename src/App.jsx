ecc98e2b789b0f68432c5cff4744cc87e7131ba8
Donovan Gall <dgall6@my.wctc.edu>
2026-02-25 17:36:51 -0600

    [1.03] Implemented SignalR

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import axios from "axios";
-import { useState, useRef, useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import Country from "./components/Country";
 import {
   Theme,
@@ -9,80 +8,223 @@ import {
   Badge,
   Container,
   Grid,
-} from "@radix-ui/themes";import { SunIcon, MoonIcon } from "@radix-ui/react-icons";
+} from "@radix-ui/themes";
+import { SunIcon, MoonIcon } from "@radix-ui/react-icons";
 import "@radix-ui/themes/styles.css";
 import "./App.css";
 import NewCountry from "./components/NewCountry";
+import axios from "axios";
+import { HubConnectionBuilder } from "@microsoft/signalr";
 
 function App() {
   const [appearance, setAppearance] = useState("dark");
+  // const apiEndpoint = "https://medals-api-v1-f9egftcjdydygngg.westus3-01.azurewebsites.net/api/country";
+  const apiEndpoint = "http://localhost:5043/api/country";
+  // const hubEndpoint = "https://medals-api-v1-f9egftcjdydygngg.westus3-01.azurewebsites.net/medalsHub";
+  const hubEndpoint = "http://localhost:5043/medalsHub";
+  const [connection, setConnection] = useState(null);
   const [countries, setCountries] = useState([]);
-  const apiEndpoint = "https://medals-api-v1-f9egftcjdydygngg.westus3-01.azurewebsites.net/api/country";
-
   const medals = useRef([
     { id: 1, name: "gold", color: "#FFD700" },
     { id: 2, name: "silver", color: "#C0C0C0" },
     { id: 3, name: "bronze", color: "#CD7F32" },
   ]);
+  const latestCountries = useRef(null);
+  // latestCountries is a ref variable to countries (state)
+  // this is needed to access state variable in useEffect w/o dependency
+  latestCountries.current = countries;
 
   useEffect(() => {
-    async function fetchData() {
+    // initial data loaded here
+    async function fetchCountries() {
       const { data: fetchedCountries } = await axios.get(apiEndpoint);
-      setCountries(fetchedCountries);
+      // we need to save the original medal count values in state
+      let newCountries = [];
+      fetchedCountries.forEach((country) => {
+        let newCountry = {
+          id: country.id,
+          name: country.name,
+        };
+        medals.current.forEach((medal) => {
+          const count = country[medal.name];
+          // page_value is what is displayed on the web page
+          // saved_value is what is saved to the database
+          newCountry[medal.name] = { page_value: count, saved_value: count };
+        });
+        newCountries.push(newCountry);
+      });
+      setCountries(newCountries);
     }
-    fetchData();
+    fetchCountries();
+
+    // signalR
+    const newConnection = new HubConnectionBuilder()
+      .withUrl(hubEndpoint)
+      .withAutomaticReconnect()
+      .build();
+
+    setConnection(newConnection);
   }, []);
+
+  useEffect(() => {
+    if (connection) {
+      connection
+        .start()
+        .then(() => {
+          console.log("Connected!");
+
+          connection.on("ReceiveAddMessage", (country) => {
+            console.log(`Add: ${country.name}`);
+
+            let newCountry = {
+              id: country.id,
+              name: country.name,
+            };
+            medals.current.forEach((medal) => {
+              const count = country[medal.name];
+              newCountry[medal.name] = {
+                page_value: count,
+                saved_value: count,
+              };
+            });
+            // we need to use a reference to countries array here
+            // since this useEffect has no dependeny on countries array - it is not in scope
+            let mutableCountries = [...latestCountries.current];
+            mutableCountries = mutableCountries.concat(newCountry);
+            setCountries(mutableCountries);
+          });
+
+          connection.on("ReceiveDeleteMessage", (id) => {
+            console.log(`Delete id: ${id}`);
+
+            let mutableCountries = [...latestCountries.current];
+            mutableCountries = mutableCountries.filter((c) => c.id !== id);
+            setCountries(mutableCountries);
+          });
+
+          connection.on("ReceivePatchMessage", (country) => {
+            console.log(`Patch: ${country.name}`);
+
+
+            let updatedCountry = {
+              id: country.id,
+              name: country.name,
+            };
+            medals.current.forEach((medal) => {
+              const count = country[medal.name];
+              updatedCountry[medal.name] = {
+                page_value: count,
+                saved_value: count,
+              };
+            });
+            let mutableCountries = [...latestCountries.current];
+            const idx = mutableCountries.findIndex((c) => c.id === country.id);
+            mutableCountries[idx] = updatedCountry;
+
+            setCountries(mutableCountries);
+          });
+        })
+        .catch((e) => console.log("Connection failed: ", e));
+    }
+    // useEffect is dependent on changes to connection
+  }, [connection]);
 
   function toggleAppearance() {
     setAppearance(appearance === "light" ? "dark" : "light");
   }
-
-  // Handling gold, silver, and bronze comes later
-  const handleAdd = async (name) => {
-    const {data: post} = await axios.post(apiEndpoint, {
-      name: name,
-      gold: 0,
-      silver: 0,
-      bronze: 0,
-    });
-    setCountries(countries.concat(post));
+  async function handleAdd(name) {
+    try {
+      await axios.post(apiEndpoint, { name: name });
+    } catch (ex) {
+      if (ex.response) {
+        console.log(ex.response);
+      } else {
+        console.log("Request failed");
+      }
+    }
+    console.log("ADD");
   }
-
-  const handleDelete = async (countryId) => {
+  async function handleDelete(countryId) {
     const originalCountries = countries;
     setCountries(countries.filter((c) => c.id !== countryId));
     try {
       await axios.delete(`${apiEndpoint}/${countryId}`);
-    } catch(ex) {
+    } catch (ex) {
       if (ex.response && ex.response.status === 404) {
+        // country already deleted
         console.log(
           "The record does not exist - it may have already been deleted"
         );
       } else {
-        alert("An error occurred while deleting a country");
+        alert("An error occurred while deleting");
         setCountries(originalCountries);
       }
     }
   }
-
   function handleIncrement(countryId, medalName) {
-    const idx = countries.findIndex((c) => c.id === countryId);
-    const mutableCountries = [...countries];
-    mutableCountries[idx][medalName] += 1;
-    setCountries(mutableCountries);
+    handleUpdate(countryId, medalName, 1);
   }
-
   function handleDecrement(countryId, medalName) {
+    handleUpdate(countryId, medalName, -1);
+  }
+  function handleUpdate(countryId, medalName, factor) {
     const idx = countries.findIndex((c) => c.id === countryId);
     const mutableCountries = [...countries];
-    mutableCountries[idx][medalName] -= 1;
+    mutableCountries[idx][medalName].page_value += 1 * factor;
     setCountries(mutableCountries);
   }
+  async function handleSave(countryId) {
+    const originalCountries = countries;
 
+    const idx = countries.findIndex((c) => c.id === countryId);
+    const mutableCountries = [...countries];
+    const country = mutableCountries[idx];
+    let jsonPatch = [];
+    medals.current.forEach((medal) => {
+      if (country[medal.name].page_value !== country[medal.name].saved_value) {
+        jsonPatch.push({
+          op: "replace",
+          path: medal.name,
+          value: country[medal.name].page_value,
+        });
+        country[medal.name].saved_value = country[medal.name].page_value;
+      }
+    });
+    console.log(
+      `json patch for id: ${countryId}: ${JSON.stringify(jsonPatch)}`
+    );
+    // update state
+    setCountries(mutableCountries);
+
+    try {
+      await axios.patch(`${apiEndpoint}/${countryId}`, jsonPatch);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404) {
+        // country already deleted
+        console.log(
+          "The record does not exist - it may have already been deleted"
+        );
+      } else {
+        alert("An error occurred while updating");
+        setCountries(originalCountries);
+      }
+    }
+  }
+  function handleReset(countryId) {
+    // to reset, make page value the same as the saved value
+    const idx = countries.findIndex((c) => c.id === countryId);
+    const mutableCountries = [...countries];
+    const country = mutableCountries[idx];
+    medals.current.forEach((medal) => {
+      country[medal.name].page_value = country[medal.name].saved_value;
+    });
+    setCountries(mutableCountries);
+  }
   function getAllMedalsTotal() {
     let sum = 0;
+    // use medal count displayed in the web page for medal count totals
     medals.current.forEach((medal) => {
-      sum += countries.reduce((a, b) => a + b[medal.name], 0);
+      sum += countries.reduce((a, b) => a + b[medal.name].page_value, 0);
     });
     return sum;
   }
@@ -106,7 +248,7 @@ function App() {
         <NewCountry onAdd={handleAdd} />
       </Flex>
       <Container className="bg"></Container>
-            <Grid pt="2" gap="2" className="grid-container">
+      <Grid pt="2" gap="2" className="grid-container">
         {countries
           .sort((a, b) => a.name.localeCompare(b.name))
           .map((country) => (
@@ -115,6 +257,8 @@ function App() {
               country={country}
               medals={medals.current}
               onDelete={handleDelete}
+              onSave={handleSave}
+              onReset={handleReset}
               onIncrement={handleIncrement}
               onDecrement={handleDecrement}
             />
